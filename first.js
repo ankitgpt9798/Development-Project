32462f9cfe206c389ec78ba49b422676b1aacdf1
Ankit Gupta <ankit9798739110@gmail.com>
2025-12-15 22:17:26 +0530

    My first commit

--- first.js
+++ first.js
@@ -1,5 +1,7 @@
 const name="Ankit";
-const age =200;
-const balance=350;
+// const age =200;
+// const balance=350;
 
-const amount = 4500;
+// const amount = 4500;
+
+// console.log("Hello coder army");
